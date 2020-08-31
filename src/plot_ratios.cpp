d34de22363fdaceecbe1c03ff4544f70281f3999
Artur Sztuc <a.sztuc@ucl.ac.uk>
2020-08-31 12:15:48 -0500

    Changing ratio plots to black -- too many plots

--- src/plot_ratios.cpp
+++ src/plot_ratios.cpp
@@ -18,13 +18,15 @@ int main(int argc, char *argv[])
 {
   // Input
   if (argc < 2){
-    std::cout << "USAGE: ./plot TTree_1 [TTree_2, TTree_3 .... TTree_N]" << std::endl;
+    std::cout << "USAGE: ./plot mode TTree_1 [TTree_2, TTree_3 .... TTree_N]" << std::endl;
   }
   for(int arg = 0; arg < argc; ++arg){
     std::cout << argv[arg] << std::endl;
   }
+
+  int mode = atoi(argv[1]);
   
-  std::string input1(argv[1]);
+  std::string input1(argv[2]);
   TFile *fin1 = new TFile(input1.c_str(), "READ");
   TTree *tin1 = (TTree*)fin1->Get("beam_monitors");
 
@@ -37,18 +39,19 @@ int main(int argc, char *argv[])
   plot1.setTimePlots();
   plot1.setDisplayPlots();
 
+  plot1.SetHornMode(mode);
   // Draw all the plots
-  plot1.drawRatioPlots(kBlack);
-  plot1.drawTimePlots(kBlack);
-  plot1.drawDisplayPlots(kBlack);
+  plot1.drawRatioPlots(kBlack, 0);
+  plot1.drawTimePlots(kBlack, 0);
+  plot1.drawDisplayPlots(kBlack, 0);
 
 
 
-  if(argc > 2){
+  if(argc > 3){
       TFile *fin2;
       TTree *tin2;
     
-    for(int plt = 2; plt < argc; ++plt){
+    for(int plt = 3; plt < argc; ++plt){
       std::cout << plt << std::endl;
       std::string input2(argv[plt]);
       fin2 = new TFile(input2.c_str(), "READ");
@@ -58,9 +61,9 @@ int main(int argc, char *argv[])
       plot1.fillRAM();
 
       // Draw again
-      plot1.drawRatioPlots(plt, 1);
-      plot1.drawTimePlots(plt, 1);
-      plot1.drawDisplayPlots(plt, 1);
+      plot1.drawRatioPlots(kBlack , 1);
+      plot1.drawTimePlots(kBlack, 1);
+      plot1.drawDisplayPlots(kBlack, 1);
 
       //delete fin2;
       plot1.clearRAM();
